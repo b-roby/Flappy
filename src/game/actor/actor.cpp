2e41cf06b923d20ba64911c9d517da333ba4bb3e
roby <robertantosica@gmail.com>
2021-06-13 21:56:28 +0300

    da

--- src/game/actor/actor.cpp
+++ src/game/actor/actor.cpp
@@ -1,7 +1,7 @@
 #include "actor.h"
 
 
-Actor::Actor(sf::String const& texture_file_name, float x_velocity, float y_velocity) {
+Actor::Actor(sf::String const& texture_file_name, float x_velocity, float y_velocity, bool should_tick) {
 
 //    texture_->loadFromFile(base_resources_path + texture_file_name);
 //    sprite_->setTexture(*texture_);
@@ -9,12 +9,11 @@ Actor::Actor(sf::String const& texture_file_name, float x_velocity, float y_velo
     this->x_velocity = x_velocity;
     this->y_velocity = y_velocity;
 
+    this->should_tick = should_tick;
+
 }
 
 void Actor::Move() {
-    sprite_->move(x_velocity, y_velocity);
+    shape.move(x_velocity * delta_time, y_velocity * delta_time);
 }
 
-void Actor::Tick() {
-    Move();
-}
