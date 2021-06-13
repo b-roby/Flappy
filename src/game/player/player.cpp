684cc56bd12234cadaff0687107cc833cd5351d8
roby <robertantosica@gmail.com>
2021-06-13 21:56:21 +0300

    da

--- src/game/player/player.cpp
+++ src/game/player/player.cpp
@@ -1,11 +1,12 @@
+#include <iostream>
 #include "player.h"
 
-Player::Player(sf::String const& textureFileName, float x_velocity, float y_velocity)
-            : Actor(textureFileName, x_velocity, y_velocity) {
+Player::Player(sf::String const& textureFileName, float x_velocity, float y_velocity, bool should_tick)
+            : Actor(textureFileName, x_velocity, y_velocity, should_tick) {
     shape = sf::RectangleShape(sf::Vector2f(50, 50));
 }
 
 void Player::Tick() {
-//    Move();
-    shape.move(x_velocity, y_velocity);
+    delta_time = clock.restart().asSeconds();
+    Move();
 }
